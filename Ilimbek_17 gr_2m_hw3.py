4d2cde4bca81a24b24192770830df5f338f7a174
Ilimbek1101 <103297357+Ilimbek1101@users.noreply.github.com>
2022-04-14 11:23:58 +0600

    first commit

--- Ilimbek_17 gr_2m_hw3.py
+++ Ilimbek_17 gr_2m_hw3.py
@@ -1,5 +1,5 @@
 # Выполнение домашнего задания 3, ООП (2 месяц).
-# .
+# Операции с дробями и палиндромные числа.
 
 class Fraction:
 
@@ -7,32 +7,40 @@ class Fraction:
         self.numertor = numertor
         self.denumerator = denumerator
 
+    def _get_fraction(self):
+        print(f'{self.numertor} / {self.denumerator}')
+
     def __add__(self, other):
         print("Is Dunder Method __add__")
-        num = self.numertor/self.denumerator + other
-        print(num)
+        self.numertor = num1.numertor * num2.denumerator + num2.numertor * num1.denumerator
+        self.denumerator = num1.denumerator * num2.denumerator
+        self._get_fraction()
 
     def __sub__(self, other):
         print("Is Dunder Method __sub__")
-        num = self.numertor / self.denumerator - other
-        print(num)
+        self.numertor = num1.numertor * num2.denumerator - num2.numertor * num1.denumerator
+        self.denumerator = num1.denumerator * num2.denumerator
+        self._get_fraction()
 
     def __mul__(self, other):
         print("Its Dunder Method __mul__")
-        num = self.numertor / self.denumerator * other
-        print(num)
+        self.numertor = num1.numertor * num2.numertor
+        self.denumerator = num1.denumerator * num2.denumerator
+        self._get_fraction()
 
     def __floordiv__(self, other):
         print("Its Dunder Method __floordiv__")
-        num = int(self.numertor / self.denumerator) // other
-        print(num)
+        self.numertor = num1.numertor * num2.denumerator
+        self.denumerator = num1.denumerator * num2.numertor
+        self._get_fraction()
 
-num = Fraction(92, 15)
+num1 = Fraction(2, 3)
+num2 = Fraction(4, 5)
 
-num + 10
-num - 11
-num * 60
-num // 3
+num1 + num2
+# num1 - num2
+# num1 * num2
+# num1 // num2
 
 
 # ExtraTask:
